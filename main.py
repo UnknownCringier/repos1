4efc0df852d16f14b5cb1d100e811a9e3191e980
pc3 <vasvladserg1@gmail.com>
2022-04-22 09:26:08 +0300

    example

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -10,9 +7,6 @@ def print_hi(name):
     print(f' ja e cring')
     print(f'1242142113')
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
